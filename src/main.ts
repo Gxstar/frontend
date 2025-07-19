e602aea5b1c5460f6a236fc3da9d1d55c18f0783
gxstar <gengxing123@qq.com>
2025-07-19 15:19:24 +0800

    feat: 添加OpenAPI基础路径配置
    在开发环境下使用代理路径，在生产环境下使用实际的后端地址。添加了OpenAPI.BASE的基础配置，便于在不同环境中切换API请求地址。

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { OpenAPI } from './services/api/core/OpenAPI';
 import App from './App.vue'
 import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './assets/main.css'
+
+// 在开发环境下使用代理路径，在生产环境下使用实际的后端地址
+
+OpenAPI.BASE = 'http://127.0.0.1:8000'; // 替换为你的生产后端地址
+
+
 const app = createApp(App)
 
 app.use(createPinia())
